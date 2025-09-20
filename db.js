446b6d2e6877632f39207590610d6f57843fa217
Hirou41 <129115317+Hirou41@users.noreply.github.com>
2025-09-20 15:13:27 -0300

    Update db.js

--- db.js
+++ db.js
@@ -60,7 +60,7 @@ const produtos = [
 ];
 
 // Imagem padrão para brigadeiros sem foto
-const IMAGEM_PADRAO = "../CheiroDeRosa//Foto Brigadeiro/Caixa-brigadeiro.jpeg";
+const IMAGEM_PADRAO = "../CheiroDeRosa/Foto Brigadeiro/Caixa-brigadeiro.jpeg";
 
 // Função para renderizar produtos filtrados
 function renderizarProdutos(filtro = "Todos", busca = "") {
